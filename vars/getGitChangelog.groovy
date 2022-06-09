05f613ea68ed81a75c7c665f27c8d1f415da4cbe
NikitaBolshakov <bns05052001@gmail.com>
2022-06-09 12:22:56 +0300

    fix(): getGitChangelog command

--- vars/getGitChangelog.groovy
+++ vars/getGitChangelog.groovy
@@ -1,24 +1,5 @@
-import me.bolshakov.gitChangelog.Change
-
-@NonCPS
-def getChangeSets(build){
-    build.changeSets
-}
+import me.bolshakov.gitChangelog.ChangeSet
 
 def call(builds){
-    def changes = []
-    for (int x = 0; x < builds.size(); x++) {
-        def currentBuild = builds[x];
-        def changeLogSets = getChangeSets(currentBuild)
-        for (int i = 0; i < changeLogSets.size(); i++) {
-            def entries = changeLogSets[i].items
-            for (int j = 0; j < entries.length; j++) {
-                entry = entries[j]
-                String author = entry.author
-                def currentChange = new Change(author, entry.msg as String, 0);
-                changes.add(currentChange);
-            }
-        }
-    }
-    changes
+    ChangeSet.getChanges(builds)
 }
