353204c0fc292af9f796bcb08f4a143b53334db3
NikitaBolshakov <bns05052001@gmail.com>
2022-06-15 17:09:35 +0300

    fix(): usc command

--- vars/usc.groovy
+++ vars/usc.groovy
@@ -1,7 +1,9 @@
 def call(args){
+    returnStatus = args?.returnStatus ? args.returnStatus : true
+    returnStdout = args?.returnStdout ? args.returnStdout : false
     if(isUnix()){
-        sh(script:args.script, encoding:args?.encoding, label:args?.label, returnStatus:args?.returnStatus, returnStdout:args?.returnStdout)
+        sh(script:args.script, encoding:args?.encoding, label:args?.label, returnStatus:returnStatus, returnStdout:returnStdout)
     }
     else
-        bat(script:args.script, encoding:args?.encoding, label:args?.label, returnStatus:args?.returnStatus, returnStdout:args?.returnStdout)
+        bat(script:args.script, encoding:args?.encoding, label:args?.label, returnStatus:returnStatus, returnStdout:returnStdout)
 }
