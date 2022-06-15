4175413f162d544299fbb92c348f54dded7de919
NikitaBolshakov <bns05052001@gmail.com>
2022-06-15 17:17:43 +0300

    feat(): usc command

--- vars/usc.groovy
+++ vars/usc.groovy
@@ -1,10 +1,23 @@
 def call(args){
-    script = args instanceof String ? args : args.script
-    returnStatus = args?.returnStatus ? args.returnStatus : true
-    returnStdout = args?.returnStdout ? args.returnStdout : false
+    if(args instanceof String)
+    {
+        script = args
+        returnStatus = true
+        returnStdout = false
+        encoding = null
+        label = null
+    }
+    else{
+        script = args.script
+        returnStatus = args?.returnStatus ? args.returnStatus : true
+        returnStdout = args?.returnStdout ? args.returnStdout : false
+        encoding = args?.encoding
+        label = args?.label
+    }
+
     if(isUnix()){
-        sh(script:script, encoding:args?.encoding, label:args?.label, returnStatus:returnStatus, returnStdout:returnStdout)
+        sh(script:script, encoding:encoding, label:label, returnStatus:returnStatus, returnStdout:returnStdout)
     }
     else
-        bat(script:script, encoding:args?.encoding, label:args?.label, returnStatus:returnStatus, returnStdout:returnStdout)
+        bat(script:script, encoding:encoding, label:label, returnStatus:returnStatus, returnStdout:returnStdout)
 }
