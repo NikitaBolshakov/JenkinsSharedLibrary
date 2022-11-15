b9800d8b2d779d19c0f97e9158d860b98236850f
NikitaBolshakov <58260141+NikitaBolshakov@users.noreply.github.com>
2022-11-15 15:09:37 +0300

    Update uploadToGitlab.groovy

--- vars/uploadToGitlab.groovy
+++ vars/uploadToGitlab.groovy
@@ -1,5 +1,6 @@
 def call(args) {
     withCredentials([string(credentialsId: args.credentialsId, variable: "token")]) {
-        usc script: """curl -k --header "PRIVATE-TOKEN: ${token}" --upload-file ${args.sourcePath} "${args.url}/${args.packageName}/${args.version}/${args.fileNameWithExtension}" """
+        file_path = findFiles "${args.sourcePath}"
+        usc script: """curl -k --header "PRIVATE-TOKEN: ${token}" --upload-file ${file_path} "${args.url}/${args.packageName}/${args.version}/${args.fileNameWithExtension}" """
     }
 }
